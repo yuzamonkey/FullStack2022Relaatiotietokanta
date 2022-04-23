6e5dfcc86ade60f5c764307dd8e7266dde28dc8b
Jusa Mankki <jusamankki@gmail.com>
2022-04-23 13:25:48 +0300

    exc4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
@@ -12,26 +13,71 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
 })
 
+class Blog extends Model { }
+Blog.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.TEXT,
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog'
+})
+Blog.sync()
+
 app.get('/api/blogs', async (req, res) => {
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    // const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    const blogs = await Blog.findAll()
     res.json(blogs)
 })
+
+app.get('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        res.json(blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/blogs', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body)
+        res.json(blog)
+    } catch (error) {
+        return res.status(400).json({ error })
+    }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        blog.destroy()
+        return res.status(200).json('ok')
+    } else {
+        return res.status(400).json(`id ${req.params.id} not valid`)
+    }
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-// const main = async () => {
-//   try {
-//     await sequelize.authenticate()
-//     const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-//     blogs.map(b => {
-//         console.log(`${b.author}: ${b.title}, ${b.likes}`)
-//     })
-//     sequelize.close()
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error)
-//   }
-// }
-
-// main()
