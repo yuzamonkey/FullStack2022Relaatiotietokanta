7ae51c101003d7606c2686cdcd1a920a996f0bd0
Jusa Mankki <jusamankki@gmail.com>
2022-04-23 12:50:01 +0300

    create web app

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
+app.get('/api/blogs', async (req, res) => {
     const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    blogs.map(b => {
-        console.log(`${b.author}: ${b.title}, ${b.likes}`)
-    })
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+    res.json(blogs)
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
-main()
+// const main = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+//     blogs.map(b => {
+//         console.log(`${b.author}: ${b.title}, ${b.likes}`)
+//     })
+//     sequelize.close()
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error)
+//   }
+// }
+
+// main()
