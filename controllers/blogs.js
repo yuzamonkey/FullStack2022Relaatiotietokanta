bb05997f6f30a81332c6d843717c7204f708fcdb
Jusa Mankki <jusamankki@gmail.com>
2022-04-23 14:20:33 +0300

    middleware & exc6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,14 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
-})
-
-router.get('/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-        res.json(blog)
-    } else {
-        res.status(404).end()
-    }
 })
 
 router.post('/', async (req, res) => {
@@ -25,10 +21,27 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-        blog.destroy()
+router.get('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        res.json(req.blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        req.blog.likes = req.body.likes
+        await req.blog.save()
+        res.json(req.blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        req.blog.destroy()
         return res.status(200).json('ok')
     } else {
         return res.status(400).json(`id ${req.params.id} not valid`)
