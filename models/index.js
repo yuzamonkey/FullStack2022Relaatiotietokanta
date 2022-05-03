775315402bbd68816abb16d7713a1cd8a2bb0521
Jusa Mankki <jusamankki@gmail.com>
2022-05-03 08:55:49 +0300

    exc19

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const Readinglist = require('./readinglist')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-// Blog.sync({ alter: true })
-// User.sync({ alter: true })
+Readinglist.hasMany(Blog)
+Readinglist.belongsTo(User)
 
 module.exports = {
   Blog,
-  User
+  User,
+  Readinglist
 }
